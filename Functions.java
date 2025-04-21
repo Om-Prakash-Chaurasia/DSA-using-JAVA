0da44dc9d531a89def5ade20cbd94a787227fef4
Om-Prakash-Chaurasia <omchaurasia2024@gmail.com>
2025-04-21 18:46:16 +0530

    first commit

---
+++ Functions.java
@@ -0,0 +1,555 @@
+// FUNCTIONS IN JAVA
+
+/*
+ * Functions are block of codes in Java which takes some inputs and returns us
+ * the output by performing some operation
+ * syntax for writing a function:-
+ * returnType functionName(type arg1, type arg2...) {
+ * operations
+ * }
+ * 
+ * Return type can be integer or float or double or void whichever data-types we
+ * have studied.
+ * Here 'void' is the type which means 'no return'
+ * But return type should be of only one type.
+ * 
+ * e.g. public static void main (String[] args) {}
+ * Here: we will study about 'public' and 'static' keyword in further classes
+ * but, for now in this, 'void' is the 'returnType', 'main' is the
+ * 'functionName'. 'functionName' can be anything but not 'keywords' of Java.
+ * 
+ * After functionName we pass the arguments in '()' along with its dataType like
+ * int, float, double, string, etc.. There can be any number of arguments.
+ * e.g. public static int personDetail(String name, int age) {
+ * operations
+ * }
+ * 
+ * NOTE: The 'functionName' should be in camelCase.
+ */
+/*--------------------------------------------------------*/
+
+// EXAMPLES OF FUNCTIONS
+
+// Qs. Print a given name in a function:-
+
+// Solution:
+// import java.util.*;
+
+// class Strings {
+// public static void printMyName(String name) {
+// System.out.println(name);
+// return;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// String name = sc.nextLine();
+
+// printMyName(name); // function call
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// Qs. Make a function to add 2 numbers and return the sum:-
+
+// Solution:
+// import java.util.*;
+
+// class Sum {
+// public static int calculateSum(int a, int b) {
+// int sum = a + b;
+// return sum;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+
+// int sum = calculateSum(a, b);
+// System.out.println(sum);
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// Qs. Make a function to multiply 2 numbers and return their product:-
+
+// Solution:
+// import java.util.*;
+
+// class Product {
+// public static int calculateProduct(int a, int b) {
+
+// return a * b;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+
+// System.out.println(calculateProduct(a, b));
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// Qs. Find the factorial of a number:-
+
+// Solution:
+// import java.util.*;
+
+// class Factorial {
+// public static void printFactorial(int n) {
+
+// if (n < 0) {
+// System.out.println("Invalid Number");
+// return;
+// }
+// int factorial = 1;
+// for (int i = n; i >= 1; i--) {
+// factorial *= i;
+// }
+// System.out.println(factorial);
+// return;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+
+// printFactorial(n);
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+/*
+ * What is the difference between functions and methods?
+ * Functions and methods does the same work but the only difference is that
+ * 'functions' are called 'directly' whereas, 'methods' are called 'through
+ * class objects'.
+ */
+/*--------------------------------------------------------*/
+
+// Qs. Make a function to check if a number is prime or not:-
+
+// Solution:
+// import java.util.*;
+
+// class IsPrime {
+// public static boolean isPrime(int n) {
+
+// boolean flag = false;
+// for (int i = 2; i <= n / 2; i++) {
+// if (n % i == 0) {
+// flag = true;
+// break;
+// }
+// }
+
+// if (!flag) {
+// System.out.println(n + " is prime");
+// } else {
+// System.out.println(n + " is not prime");
+// }
+
+// return flag;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// isPrime(n);
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// Qs. Make a function to check if a given number is even or not:-
+
+// Solution:
+// import java.util.*;
+
+// class EvenOrOdd {
+// public static boolean isEven(int n) {
+// boolean flag = false;
+// if (n % 2 == 0) {
+// flag = true;
+// }
+
+// if (flag) {
+// System.out.println(n + " is even");
+// } else {
+// System.out.println(n + " is not even");
+// }
+
+// return flag;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// isEven(n);
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// Qs. Make a function to print the table of a given number n:-
+
+// Solution:
+// import java.util.*;
+
+// class PrintTable {
+// public static void printTable(int n) {
+
+// for (int i = 1; i <= 10; i++) {
+// System.out.println(n * i);
+// }
+
+// return;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// printTable(n);
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 1. Enter 3 numbers from the user & make a function to print their average:-
+
+// Solution:
+// import java.util.*;
+
+// class Average {
+// public static int average(int a, int b, int c) {
+// return (a + b + c) / 3;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+// int c = sc.nextInt();
+
+// System.out.println(average(a, b, c));
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 2. Write a function to print the sum of all odd numbers from 1 to n:-
+
+// Solution:
+// import java.util.*;
+
+// class SumOfOdds {
+// public static int sumOfOdds(int n) {
+// int sum = 0;
+// for (int i = 1; i <= n; i++) {
+// if (i % 2 != 0) {
+// sum += i;
+// }
+// }
+// return sum;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// System.out.println(sumOfOdds(n));
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 3. Write a function which takes in 2 numbers and returns the greater of those
+// two:-
+
+// Solution:
+// import java.util.*;
+
+// class Greater {
+// public static int greaterNumber(int a, int b) {
+// if (a > b) {
+// return a;
+// } else {
+// return b;
+// }
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+
+// System.out.println(greaterNumber(a, b));
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 4. Write a function that takes in the radius as input and returns the
+// circumference of a circle:-
+
+// Solution:
+// import java.util.*;
+
+// class Circumference {
+// public static double circumference(int r) {
+// return (2 * 3.14 * r);
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int r = sc.nextInt();
+
+// System.out.println(circumference(r));
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 5. Write a function that takes in age as input and returns if that person is
+// eligible to vote or not. A person of age > 18 is eligible to vote:-
+
+// Solution:
+// import java.util.*;
+
+// class Vote {
+// public static void canVote(int age) {
+// if (age > 18) {
+// System.out.println("Yes, is eligible");
+// } else {
+// System.out.println("No, is not eligible");
+// }
+
+// return;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int age = sc.nextInt();
+
+// canVote(age);
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 6. Write an infinite loop using do while condition:-
+
+// Solution:
+// import java.util.*;
+
+// class DoWhile {
+// public static void main(String[] args) {
+// int i = 0;
+// do {
+// System.out.println("Om Prakash Chaurasia");
+// i++;
+// } while (i > 0);
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 7. Write a program to enter the numbers till the user wants and at the end it
+// should display the count of positive, negative and zeros entered:-
+
+// Solution:
+// import java.util.*;
+
+// class Counts {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int positive = 0;
+// int negative = 0;
+// int zeros = 0;
+
+// System.out.println("Press 1 to continue or 0 to stop");
+
+// int input = sc.nextInt();
+
+// while (input == 1) {
+// System.out.println("Enter your number: ");
+// int number = sc.nextInt();
+
+// if (number > 0) {
+// positive++;
+// } else if (number < 0) {
+// negative++;
+// } else {
+// zeros++;
+// }
+
+// System.out.println("Press 1 to continue or 0 to stop");
+// input = sc.nextInt();
+// }
+
+// System.out.println("Positives: " + positive);
+// System.out.println("Negatives: " + negative);
+// System.out.println("Zeros: " + zeros);
+
+// sc.close();
+// }
+// }
+
+// In this program after every input we are asking user whether he/ she wants to
+// continue or not.
+/*--------------------------------------------------------*/
+
+// 8. Two numbers are entered by the user, x and n. Write a function to find the
+// value of one number raised to the power of another i.e. x^n:-
+
+// Solution:
+// import java.util.*;
+
+// class PowerOfX {
+// public static double power(int x, int n) {
+// double pow = Math.pow(x, n);
+
+// return pow;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int x = sc.nextInt();
+// int n = sc.nextInt();
+
+// System.out.println(power(x, n));
+
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+// 9. Write a function that calculates the Greatest Common Divisor of 2 numbers.
+
+// Solution:
+// import java.util.*;
+
+// class GCD {
+// public static int greatestCommonDivisor(int n1, int n2) {
+// int gcd = 0;
+// for (int i = 1; i <= n1 && i <= n2; i++) {
+// if (n1 % i == 0 && n2 % i == 0) {
+// gcd = i;
+// }
+// }
+// return gcd;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int n1 = sc.nextInt();
+// int n2 = sc.nextInt();
+
+// System.out.println(greatestCommonDivisor(n1, n2));
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
+
+/*
+ * 10. Write a program to print Fibonacci series of n terms where n is input by
+ * user:
+ * 0 1 1 2 3 5 8 13 21...
+ * In the Fibonacci series, a number is the sum of the previous 2 numbers that
+ * came before it.
+ */
+
+// Solution:
+// import java.util.*;
+
+// class Fibonacci {
+// public static int fibonacciNumbers(int n) {
+// int sum = 0;
+// int a = 0;
+// int b = 1;
+
+// System.out.println(a + " ");
+
+// for (int i = 2; i <= n; i++) {
+// System.out.println(b + " ");
+// sum = a + b;
+// a = b;
+// b = sum;
+// }
+// return sum;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// fibonacciNumbers(n);
+
+// sc.close();
+// }
+// }
+
+// OR
+
+// import java.util.*;
+
+// class Fibonacci {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+
+// int a = 0, b = 1;
+
+// System.out.print(a + " ");
+
+// if (n > 1) {
+// // find nth term
+// for (int i = 2; i <= n; i++) {
+// System.out.print(b + " ");
+// // the concept below is called swapping
+// int temp = b;
+// b = a + b;
+// a = temp;
+// }
+
+// System.out.println();
+// }
+// sc.close();
+// }
+// }
+/*--------------------------------------------------------*/
---
+++ Sortings.java
@@ -0,0 +1,298 @@
+/*
+ * Sorting in Java :- Either sorting elements from highest to lowest value
+ * (descending order) or from lowest to highest value (ascending order).
+ * 
+ * Types of Sorting :- These are the most common types of sorting.
+ * 1. Bubble sort :- This technique works by repeatedly swapping the adjacent
+ * elements if they are in the wrong order. This algorithm is not suitable for
+ * large data sets as its average and worst-case time complexity is quiet high
+ * i.e. TC = O(n^2).
+ * 
+ * Algorithm :-
+ * 
+ * i. Traverse from left and compare adjacent elements and the higher one is
+ * placed at right side.
+ * ii. In this way, the largest element is moved to the rightmost end at first.
+ * iii. This process is then continued ton find the second largest and place it
+ * and so on until the data is sorted.
+ * 
+ * e.g. is shown below.
+ */
+
+// class BubbleSorting {
+// public static void printArray(int arr[]) {
+// for (int i = 0; i < arr.length; i++) {
+// System.out.print(arr[i] + " ");
+// }
+// System.out.println();
+// }
+
+// public static void main(String[] args) {
+// int arr[] = { 7, 8, 3, 1, 2 };
+// // Bubble sort
+// for (int i = 0; i < arr.length; i++) {
+// for (int j = 0; j < arr.length - i - 1; j++) {
+// if (arr[j] > arr[j + 1]) {
+// // Swap
+// int temp = arr[j];
+// arr[i = j] = arr[j + 1];
+// arr[j + 1] = temp;
+// }
+// }
+// }
+// printArray(arr);
+// }
+// }
+/*---------------------------------------------------------------- */
+
+/*
+ * 2. Selection sort :- This is a simple and efficient sorting that works by
+ * repeatedly selecting the smallest (or largest) element from the unsorted
+ * portion of the list and moving it to the sorted portion of the list. TC = O(n
+ * ^ 2).
+ * 
+ * Algorithm :-
+ * The algorithm repeatedly selects the smallest (or largest) element from the
+ * unsorted portion of the list and swaps it with the first element of the
+ * unsorted part. This process is repeated for the remaining unsorted portion
+ * until the entire list is sorted.
+ * 
+ * e.g. is shown below.
+ */
+
+// class SelectionSort {
+// public static void printArray(int arr[]) {
+// for (int i = 0; i < arr.length; i++) {
+// System.out.print(arr[i] + " ");
+// }
+// System.out.println();
+// }
+
+// public static void main(String[] args) {
+// int arr[] = { 7, 8, 3, 1, 2 };
+
+// // Selection sort
+// for (int i = 0; i < arr.length - 1; i++) {
+// int smallest = i;
+// for (int j = i + 1; j < arr.length; j++) {
+// if (arr[smallest] > arr[j]) {
+// smallest = j;
+// }
+// }
+// int temp = arr[smallest];
+// arr[smallest] = arr[i];
+// arr[i] = temp;
+// }
+// printArray(arr);
+// }
+// }
+/*---------------------------------------------------------------- */
+
+/*
+ * 3. Insertion sort :- This is a simple sorting algorithm that works similar to
+ * the way you sort playing cards in your hands. The array is virtually split
+ * into a sorted and an unsorted part. Values from the unsorted part are picked
+ * and placed at the correct position in the sorted part. TC = O(n ^ 2).
+ * 
+ * Algorithm :-
+ * To sort an array of size N in ascending order iterate over the array and
+ * compare the current element (key) to its predecessor, if the key element is
+ * smaller than its predecessor, compare it to the elements before. Move the
+ * greater elements one position up to make space for the swapped element.
+ * 
+ * e.g. is shown below.
+ */
+
+// class InsertionSort {
+// public static void printArray(int arr[]) {
+// for (int i = 0; i < arr.length; i++) {
+// System.out.print(arr[i] + " ");
+// }
+// System.out.println();
+// }
+
+// public static void main(String[] args) {
+// int arr[] = { 7, 8, 3, 1, 2 };
+
+// // Insertion sort
+// for (int i = 1; i < arr.length; i++) {
+// int current = arr[i];
+// int j = i - 1;
+// while (j >= 0 && current < arr[j]) {
+// arr[j + 1] = arr[j];
+// j--;
+// }
+// // placement
+// arr[j + 1] = current;
+// }
+// printArray(arr);
+// }
+// }
+/*---------------------------------------------------------------- */
+
+/*
+ * 4. Merge sort :- This is a sorting algorithm that works by dividing an array
+ * (divide and conquer) into smaller sub-arrays, sorting each sub-array, and
+ * then merging the sorted sub-arrays back together to form the final sorted
+ * array. TC = O(nlogn).
+ * 
+ * Algorithm :-
+ * Merge sort is a recursive algorithm that continuously splits the
+ * array in half until it cannot be further divided i.e. the array has only one
+ * element left (an array with one element is always sorted). Then the sorted
+ * sub-arrays are merged into one sorted array.
+ * 
+ * e.g. is shown below.
+ */
+
+// class MergeSort {
+// public static void conquer(int arr[], int si, int mid, int ei) {
+// // TC for conquer = O(n)
+// int merged[] = new int[ei - si + 1];
+
+// int idx1 = si; // This is tracking the 1st array.
+// int idx2 = mid + 1; // This is tracking the second array.
+// int x = 0; // This is tracking the merged array.
+
+// while (idx1 <= mid && idx2 <= ei) {
+// if (arr[idx1] <= arr[idx2]) {
+// // merged[x] = arr[idx1];
+// // x++; idx1++;
+// // These two lines can also be written as;
+// merged[x++] = arr[idx1++];
+// } else {
+// merged[x++] = arr[idx2++];
+// }
+// }
+
+// // If either of the condition is false in above while loop the either of the
+// // below while loop will execute.
+// while (idx1 <= mid) {
+// merged[x++] = arr[idx1++];
+// }
+
+// while (idx2 <= ei) {
+// merged[x++] = arr[idx2++];
+// }
+
+// // To copy the elements of merged array into original array.
+// for (int i = 0, j = si; i < merged.length; i++, j++) {
+// arr[j] = merged[i];
+// }
+// }
+
+// public static void divide(int arr[], int si, int ei) {
+// // TC for divide = O(logn)
+// if (si >= ei) {
+// return;
+// }
+
+// // int mid = (si + ei) / 2; // But this formula causes TC to increase so we
+// use;
+// int mid = si + (ei - si) / 2;
+// divide(arr, si, mid);
+// divide(arr, mid + 1, ei);
+// conquer(arr, si, mid, ei);
+// }
+
+// public static void main(String[] args) {
+// // Overall TC = O(nlogn)
+
+// int arr[] = { 6, 3, 9, 5, 2, 8 };
+// int n = arr.length;
+
+// divide(arr, 0, n - 1);
+
+// // Print
+// for (int i = 0; i < n; i++) {
+// System.out.print(arr[i] + " ");
+// }
+// System.out.println();
+// }
+// }
+/*---------------------------------------------------------------- */
+
+/*
+ * 5. Quick sort :- This sorting algorithm is based on the Divide and Conquer
+ * algorithm that picks an element as a pivot and partitions the given array
+ * around the picked pivot by placing the pivot in its correct position in the
+ * sorted array. Average TC = O(nlogn) and Worst TC = O(n^2). The worst TC
+ * occurs when the array which we are trying to sort is already sorted either in
+ * ascending order or descending order.
+ * 
+ * Algorithm :-
+ * The key process in quickSort is a partition(). The target of partitions is to
+ * place the pivot (any element can be chosen to be a pivot) at its correct
+ * position in the sorted array and put all smaller elements to the left of the
+ * pivot, and all greater elements to the right of the pivot.
+ * Partition is done recursively on each side of the pivot after the pivot is
+ * placed in its correct position and this is finally sorts the array.
+ * 
+ * Choice of Pivot :-
+ * i. Always pick the first element as a pivot.
+ * ii. Always pick the last element as a pivot (most commonly used).
+ * iii. Pick a random element as a pivot.
+ * iv. Pick the middle as the pivot.
+ * 
+ * Partition Algorithm :-
+ * The logic is simple, we start from the leftmost element and keep track of the
+ * index of smaller (or equal) elements as i. While traversing, if we find a
+ * smaller element, we swap the current element with arr[i]. Otherwise, we
+ * ignore the current element.
+ * 
+ * e.g. is shown below.
+ */
+
+// class QuickSort {
+// public static int partition(int arr[], int low, int high) {
+// int pivot = arr[high];
+// int i = low - 1;
+
+// for (int j = low; j < high; j++) {
+// if (arr[j] < pivot) {
+// i++;
+
+// // Swap
+// int temp = arr[i];
+// arr[i] = arr[j];
+// arr[j] = temp;
+// }
+// }
+// i++;
+// int temp = arr[i];
+// arr[i] = pivot;
+// arr[high] = temp;
+// return i; // Pivot index
+// }
+
+// public static void quickSort(int arr[], int low, int high) {
+// if (low < high) {
+// int pidx = partition(arr, low, high);
+
+// quickSort(arr, low, pidx - 1);
+// quickSort(arr, pidx + 1, high);
+// }
+// }
+
+// public static void main(String[] args) {
+// int arr[] = { 6, 3, 9, 5, 2, 8 };
+// int n = arr.length;
+
+// quickSort(arr, 0, n - 1);
+
+// // Print
+// for (int i = 0; i < n; i++) {
+// System.out.print(arr[i] + " ");
+// }
+// System.out.println();
+// }
+// }
+
+/*
+ * But why do we use Quick sort then instead of Merge sort???
+ * => Because the worst TC of Quick sort may be O(n^2), but it does not take any
+ * extra memory like Merge sort. When we have extra memory which we can use then
+ * we can use Merge sort to save time but if we want to save memory then it's
+ * better to use Quick sort.
+ */
+/*---------------------------------------------------------------- */
